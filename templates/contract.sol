fc50c4af041bea2c398dbf6feebc0e860db167f2
Ouwehand, J.D. (Jeroen) <jeroen.ouwehand1@devolksbank.nl>
2019-03-06 16:32:03 +0100

    Created default contract template

---
+++ templates/contract.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.5.4;
+
+contract exampleContract {
+    bytes32 exampleString;
+
+    constructor() public {}
+
+    /**
+     * @dev this is a example function
+     * @param <your-param>
+     * @return <your-return-statement>
+     */
+    function exampleFunction() public {
+    }
+
+}
